27eaa33cdbfe3bdcdcc21301bdef930686397112
TommykhoiZ <superminhkhoi2004@gmail.com>
2024-12-31 10:31:53 +0700

    updated main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import "../app/styles/global.css";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "./images/1.png";
+import portrait from "./images/portrait.jpg";
 import ChatBox from "./components/chatbox";
 import Header from "./components/header";
 
@@ -20,16 +20,14 @@ export default function Home() {
           <section className="mb-8">
             <div className="header-container">
               <Image
-                src={logo}
-                width={150}
-                height={150}
+                src={portrait}
                 alt="Picture of the author"
                 className="profile-image"
               />
               <div className="header-content">
-                <p className="text-xl text-gray-600">AI Specialist</p>
-                <p className="">AI Specialist</p>
-                <h1 className="heading-1 animated-text">Your Name</h1>
+                <p className="text-xl text-gray-600">Aspiring AI</p>
+                <p className="">"Consistence is key to success"</p>
+                <h1 className="heading-1 animated-text">Hoang Minh Khoi</h1>
                 <div className="mt-4"></div>
               </div>
             </div>
@@ -37,28 +35,20 @@ export default function Home() {
               <p>
                 Email:{" "}
                 <a href="hoangminhkhoi3018@gmail.com" className="text-blue-500">
-                  your.email@example.com
+                  hoangminhkhoi3108@gmail.com
                 </a>
               </p>
-              <p>Phone: 123-456-7890</p>
+              <p>Phone: 0422296339 </p>
               <p>
                 LinkedIn:{" "}
                 <a
-                  href="https://linkedin.com/in/yourprofile"
+                  href="https://www.linkedin.com/in/tom-hoang-3a99ba319/"
                   className="text-blue-500"
                 >
-                  linkedin.com/in/yourprofile
+                  Tom Hoang
                 </a>
               </p>
               <p>
-                <nav className="mb-6">
-                  <Link
-                    href="/dashboard"
-                    className="text-blue-500 hover:text-blue-700"
-                  >
-                    🤣🤣🤣 To Dashboard
-                  </Link>
-                </nav>
               </p>
             </div>
           </section>
@@ -67,34 +57,50 @@ export default function Home() {
               Summary
             </h2>
             <p>
-              Passionate AI specialist with experience in machine learning, deep
+              Passionate AI enthusiastic with experience in machine learning, deep
               learning, and data analysis.
             </p>
           </section>
-          <section className="mb-8">
+          <section className="mb-12">
             <h2 className="text-2xl font-semibold border-b pb-2 mb-4">
               Experience
             </h2>
             <div className="mb-4">
-              <h3 className="text-xl font-semibold">Company A - AI Engineer</h3>
-              <p className="text-gray-600">Jan 2020 - Present</p>
+              <div className="flex justify-between items-center mb-4">
+              <h3 className="text-l font-semibold mb-3">FPT Software - Full Stack Developer & AI researcher (STU)</h3>
+              <p className="text-gray-600">Oct 2023 - Jan 2024</p>
+              </div>
               <ul className="list-disc list-inside">
                 <li>
-                  Developed machine learning models for predictive analytics.
+                Built advanced regression, classification, and clustering models for research and product development
                 </li>
                 <li>
-                  Collaborated with data scientists to improve model accuracy.
+                  Collaborated with senior DevOps, achieving a 15% boost in coding efficiency
                 </li>
               </ul>
             </div>
             <div>
-              <h3 className="text-xl font-semibold">
-                Company B - Data Scientist
+              <div className="flex justify-between items-center mb-4">
+              <h3 className="text-xl font-semibold mb-3">
+              FPT Software - AI Prompt (EBS)
               </h3>
-              <p className="text-gray-600">Jun 2018 - Dec 2019</p>
+              <p className="text-gray-600">Oct 2024 - Jan 2025</p>
+              </div>
               <ul className="list-disc list-inside">
-                <li>Analyzed large datasets to extract actionable insights.</li>
-                <li>Built data pipelines for efficient data processing.</li>
+                <li>Created and optimized AI prompts for email automation, translation, and data analysis</li>
+                <li>Implemented data pipelines for more efficient processing and analytics</li>
+              </ul>
+            </div>
+            <div>
+              <div className="flex justify-between items-center mb-4">
+              <h3 className="text-xl font-semibold mb-3">
+                Twin - AI Prompt & Chatbot
+              </h3>
+              <p className="text-gray-600">Oct 2024 - Jan 2025</p>
+              </div>
+              <ul className="list-disc list-inside">
+                <li>Developed and integrated AI-driven chatbots using Google “Gemini” with vector DB and BigQuery</li>
+                <li>Implement new conversation-based features that boosted overall efficiency by 20%</li>
               </ul>
             </div>
           </section>
@@ -102,17 +108,11 @@ export default function Home() {
             <h2 className="text-2xl font-semibold border-b pb-2 mb-4">
               Education
             </h2>
-            <div className="mb-4">
+            <div className="flex justify-between items-center mb-4">
               <h3 className="text-xl font-semibold">
-                University X - MSc in Computer Science
+                Deakin University - Bachelor of Artificial Intelligence
               </h3>
-              <p className="text-gray-600">2016 - 2018</p>
-            </div>
-            <div>
-              <h3 className="text-xl font-semibold">
-                University Y - BSc in Computer Science
-              </h3>
-              <p className="text-gray-600">2012 - 2016</p>
+              <p className="text-gray-600">2023 - Current</p>
             </div>
           </section>
           <section>
@@ -124,12 +124,12 @@ export default function Home() {
               <div>
                 <div className="flex justify-between mb-1">
                   <span>Machine Learning</span>
-                  <span>80%</span>
+                  <span>85%</span>
                 </div>
                 <div className="w-full bg-gray-200 rounded-full h-2.5">
                   <div
                     className="bg-blue-600 h-2.5 rounded-full"
-                    style={{ width: "80%" }}
+                    style={{ width: "85%" }}
                   ></div>
                 </div>
               </div>
@@ -138,12 +138,12 @@ export default function Home() {
               <div>
                 <div className="flex justify-between mb-1">
                   <span>Prompting</span>
-                  <span>90%</span>
+                  <span>95%</span>
                 </div>
                 <div className="w-full bg-gray-200 rounded-full h-2.5">
                   <div
                     className="bg-blue-600 h-2.5 rounded-full"
-                    style={{ width: "90%" }}
+                    style={{ width: "95%" }}
                   ></div>
                 </div>
               </div>
@@ -166,12 +166,12 @@ export default function Home() {
               <div>
                 <div className="flex justify-between mb-1">
                   <span>Deep Learning</span>
-                  <span>90%</span>
+                  <span>85%</span>
                 </div>
                 <div className="w-full bg-gray-200 rounded-full h-2.5">
                   <div
                     className="bg-blue-600 h-2.5 rounded-full"
-                    style={{ width: "90%" }}
+                    style={{ width: "85%" }}
                   ></div>
                 </div>
               </div>
